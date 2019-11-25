297ba0c6570d90fa76d40973f1e13c474a891a55
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-11-25 11:47:05 +0800

    update visited;

--- src/contract.js
+++ src/contract.js
@@ -7,16 +7,7 @@ const { hexToInt, logger } = require('./shared')
 class Contract {
   constructor(bin) {
     this.bin = bin
-    this.paths = []
     this.execute(0)
-    logger.info(`TOTAL: ${this.paths.length}`)
-    // this.paths.forEach(p => {
-      // this.log(p)
-    // })
-  }
-
-  shouldStop(visited) {
-    return visited.length != uniq(visited).length
   }
 
   log(path) {
@@ -30,43 +21,36 @@ class Contract {
 
   execute(pc = 0, stack = [], path = [], visited = []) {
     const opcode = opcodes[this.bin[pc]]
-    if (!opcode) return
+    if (!opcode || visited.includes(pc)) return
     const { name, ins, outs } = opcode
-    path.push({
-      stack: [...stack],
-      opcode,
-      pc
-    })
+    path.push({ stack: [...stack], opcode, pc })
+    visited.push(pc)
     switch (name) {
       case 'PUSH': {
         const dataLen = this.bin[pc] - 0x5f
         const data = this.bin.slice(pc + 1, pc + 1 + dataLen)
         stack.push(data)
-        this.execute(pc + 1 + dataLen, [...stack], [...path], [...visited])
+        this.execute(pc + 1 + dataLen, [...stack], [...path], visited)
         return
       }
       case 'POP': {
         stack.pop()
-        this.execute(pc + 1, [...stack], [...path], [...visited])
+        this.execute(pc + 1, [...stack], [...path], visited)
         return
       }
       case 'JUMPI': {
-        visited.push(hash(stack))
         const [cond, label] = stack.splice(-ins) 
         const jumpdest = hexToInt(label)
-        if (this.shouldStop(visited)) return
-        this.execute(pc + 1, [...stack], [...path], [...visited])
+        this.execute(pc + 1, [...stack], [...path], visited)
         assert(this.bin[jumpdest] && opcodes[this.bin[jumpdest]].name == 'JUMPDEST')
-        this.execute(jumpdest, [...stack], [...path], [...visited])
+        this.execute(jumpdest, [...stack], [...path], visited)
         return
       }
       case 'JUMP': {
-        visited.push(hash(stack))
         const [label] = stack.splice(-ins)
         const jumpdest = hexToInt(label)
-        if (this.shouldStop(visited)) return
         assert(this.bin[jumpdest] && opcodes[this.bin[jumpdest]].name == 'JUMPDEST')
-        this.execute(jumpdest, [...stack], [...path], [...visited])
+        this.execute(jumpdest, [...stack], [...path], visited)
         return
       }
       case 'SWAP': {
@@ -76,7 +60,7 @@ class Contract {
         assert(target >= 0)
         stack[target] = stack[stack.length - 1]
         stack[stack.length - 1] = tmp
-        this.execute(pc + 1, [...stack], [...path], [...visited])
+        this.execute(pc + 1, [...stack], [...path], visited)
         return
       }
       case 'DUP': {
@@ -84,7 +68,7 @@ class Contract {
         const target = stack.length - distance
         assert(target >= 0)
         stack.push(stack[target])
-        this.execute(pc + 1, [...stack], [...path], [...visited])
+        this.execute(pc + 1, [...stack], [...path], visited)
         return
       }
       case 'REVERT':
@@ -92,7 +76,6 @@ class Contract {
       case 'RETURN':
       case 'STOP':
       case 'INVALID': {
-        this.paths.push(path)
         return
       }
       default: {
@@ -100,7 +83,7 @@ class Contract {
         range(outs).forEach(() => {
           stack.push(Buffer.from('00', 'hex'))
         })
-        this.execute(pc + 1, [...stack], [...path], [...visited])
+        this.execute(pc + 1, [...stack], [...path], visited)
         return
       }
     }
