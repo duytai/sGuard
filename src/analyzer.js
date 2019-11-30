2f1ce7d01e7d39eebfca49438444619ce2de18cf
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-11-30 21:39:22 +0800

    simple analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,9 +1,30 @@
-const { prettify } = require('./shared')
+const { prettify, logger } = require('./shared')
 
-const analyze = (value, traces) => {
-  prettify([value])
-  console.log('----')
-  prettify(traces)
+const find = (symbol, cond) => {
+  if (cond(symbol)) return [symbol]
+  const [type, name, ...params] = symbol
+  return params.reduce(
+    (agg, symbol) => [...agg, ...find(symbol, cond)],
+    []
+  )
+}
+
+const analyze = (symbol, traces) => {
+  console.log(symbol)
+  const [type, name, ...params] = symbol 
+  switch (type) {
+    case 'const': {
+      logger.info(`No dependency since wei is ${JSON.stringify(value)}`)
+      break
+    }
+    case 'symbol': {
+      const foundSymbols = find(symbol, ([type, name]) => type == 'symbol' && name == 'NUMBER')
+      if (foundSymbols.length > 0) {
+        logger.info(`Number dependency since wei is ${JSON.stringify(symbol)}`)
+      }
+      break
+    }
+  }
 }
 
 module.exports = {
