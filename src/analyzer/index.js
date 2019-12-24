a594fe8e4915efccc79c065e377eeecbc2e4ddae
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-12-24 17:15:24 +0800

    sample load/store

--- src/analyzer/index.js
+++ src/analyzer/index.js
@@ -5,14 +5,39 @@ const { prettify, logger, findSymbol } = require('../shared')
 const Memory = require('./memory')
 const Storage = require('./storage')
 
+let counter = 0
+
 const buildDependencyTree = (node, traces) => {
   const { me, childs } = node
   assert(!childs.length)
   switch (me[1]) {
     case 'MLOAD': {
+      const [loc, loadSize, traceSize] = me.slice(2)
+      const loadVariable = Memory.toVariable(loc)
+      assert(loadVariable)
+      const mstores = traces.filter(trace => ([type, name]) => name == 'MSTORE')
+      mstores.forEach(mstore => {
+        const [loc, storedValue] = mstore.slice(2)
+        const storeVariable = Memory.toVariable(loc)
+        assert(storeVariable)
+        // TODO: Need to analyze aliasing 
+        if (loadVariable.equal(storeVariable)) {
+          if (loadVariable.toString() != 'm_40') {
+            const newNode = { me: storedValue, childs: [] }
+            buildDependencyTree(newNode, traces)
+            childs.push(newNode)
+          }
+        }
+      })
       break
     }
     case 'SLOAD': {
+      console.log('///////')
+      prettify([me])
+      const [loc] = me.slice(2)
+      const variable = Storage.toVariable(loc, traces)
+      assert(variable)
+      console.log(chalk.green(variable.toString()))
       break
     }
     default: {
@@ -38,40 +63,9 @@ const analyze = (symbol, traces) => {
   const root = { me: symbol, childs: [] }
   const [type, name, ...params] = symbol
   buildDependencyTree(root, traces)
+  console.log('---ROOT---')
   prettifyTree(root)
 }
-
-// const analyze = (symbol, traces) => {
-  // const [type, name] = symbol
-  // switch (name) {
-    // case 'MLOAD': {
-      // console.log('///////')
-      // prettify([symbol])
-      // const [loc, loadSize, traceSize] = symbol.slice(2)
-      // const loadVariable = Memory.toVariable(loc)
-      // assert(loadVariable)
-      // console.log(chalk.green(loadVariable.toString()))
-      // const mstores = traces
-        // .filter(trace => ([type, name]) => name == 'MSTORE')
-        // .filter(mstore => {
-          // const [loc] = mstore.slice(2)
-          // const storeVariable = Memory.toVariable(loc)
-          // assert(storeVariable)
-          // return loadVariable.equal(storeVariable)
-        // })
-      // break
-    // }
-    // case 'SLOAD': {
-      // console.log('///////')
-      // prettify([symbol])
-      // const [loc] = symbol.slice(2)
-      // const variable = Storage.toVariable(loc, traces)
-      // assert(variable)
-      // console.log(chalk.green(variable.toString()))
-      // break
-    // }
-  // }
-// }
 
 module.exports = {
   analyze,
