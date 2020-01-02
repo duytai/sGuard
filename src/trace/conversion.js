e787d220cf3b7c42386d9cd4016663e50915b0d9
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-01-02 17:53:58 +0800

    update coversion

--- src/trace/conversion.js
+++ src/trace/conversion.js
@@ -39,6 +39,24 @@ const isMstore40 = (t) => {
   return isConstWithValue(t[2], 0x40)
 }
 
+const isMload0 = (t) => {
+  if (t[0] == 'const') return false
+  if (t[1] != 'MLOAD') return false
+  return isConstWithValue(t[2], 0x00)
+}
+
+const isMstore0 = (t) => {
+  if (t[0] == 'const') return false
+  if (t[1] != 'MSTORE') return false
+  return isConstWithValue(t[2], 0x00)
+}
+
+const isSha3Mload0 = (t) => {
+  if (t[0] == 'const') return false
+  if (t[1] != 'SHA3') return false
+  return isMload0(t[2])
+}
+
 const toLocalVariable = (t, trace) => {
   if (isConst(t)) return new Variable(`m_${t[1].toString(16)}`) 
   if (isMload40(t)) {
@@ -47,7 +65,7 @@ const toLocalVariable = (t, trace) => {
     const subTrace = trace
       .subTrace(traceSize[1].toNumber())
       .filter(isMstore40)
-    return new Variable(`m_${subTrace.size()}`)
+    return new Variable(`m_${subTrace.size().toString(16)}`)
   }
   const properties = []
   const stack = [t]
@@ -61,7 +79,10 @@ const toLocalVariable = (t, trace) => {
       if (!hasLeftMload && !hasRightMload) {
         assert(false, 'Need an example')
       }
-      // FIXME: both left and right contain mload then left is base  (However it could be right)
+      /*
+       * FIXME: both left and right contain mload then left is base
+       * (However it could be right)
+       * */
       const baseIdx = hasRightMload ? 1 : 0
       const base = operands[baseIdx]
       const offset = operands[1 - baseIdx]
@@ -80,8 +101,50 @@ const toLocalVariable = (t, trace) => {
   }
 }
 
-const toStateVariable = (t) => {
+const toStateVariable = (t, trace) => {
   if (isConst(t)) return new Variable(`s_${t[1].toString(16)}`) 
+  if (isSha3Mload0(t)) {
+    const [mload] = t.slice(2)
+    const [type, name, base, loadSize, traceSize] = mload
+    assert(isConst(traceSize))
+    const subTrace = trace
+      .subTrace(traceSize[1].toNumber())
+      .filter(isMstore0)
+    return new Variable(`s_${subTrace.size().toString(16)}`)
+  }
+  const properties = []
+  const stack = [t]
+  while (stack.length > 0) {
+    const [type, name, ...operands] = stack.pop()
+    prettify([t])
+    assert(name == 'ADD' || name == 'SUB', `loc is ${name}`)
+    if (name == 'ADD') {
+      const hasLeftSha3 = findSymbol(operands[0], isSha3Mload0).length > 0
+      const hasRightSha3 = findSymbol(operands[1], isSha3Mload0).length > 0
+      const constIdx = findIndex(operands, ([type]) => type == 'const')
+      if (!hasLeftSha3 && !hasRightSha3) {
+        assert(false, 'Need an example')
+      }
+      /*
+       * FIXME: both left and right contain mload then left is base
+       * (However it could be right)
+       * */
+      const baseIdx = hasLeftSha3 ? 0 : 1
+      const base = operands[baseIdx]
+      const offset = operands[1 - baseIdx]
+      if (isSha3Mload0(base)) {
+        const variable = toStateVariable(base, trace)
+        variable.addN(reverse([...properties, offset]))
+        return variable
+      } else {
+        properties.push(offset)
+        stack.push(base)
+      }
+    }
+    if (name == 'SUB') {
+      stack.push(operands[0])
+    }
+  }
 }
 
 const toVariable = (t, trace) => {
