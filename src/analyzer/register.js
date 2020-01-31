430393412832f119b3414049d2ac927a2302bcf2
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-01-31 16:50:56 +0800

    move out dnode struct

--- src/analyzer/register.js
+++ src/analyzer/register.js
@@ -3,7 +3,8 @@ const { intersection, uniqBy, uniq, assign, forEach } = require('lodash')
 const DNode = require('./dnode')
 const ConditionAnalyzer = require('./condition') 
 const StackAnalyzer = require('./stack')
-const { prettify, formatSymbol } = require('../shared')
+const { prettify, formatSymbol, findSymbol } = require('../shared')
+const { toStateVariable, toLocalVariable } = require('../variable')
 
 class RegisterAnalayzer {
   constructor({ symbol, trace, pc, ep, trackingPos }, endPoints, visited = []) {
@@ -11,7 +12,8 @@ class RegisterAnalayzer {
     visited.push(this.toVisitedKey(pc, trackingPos, symbol))
     this.condAnalyzer = new ConditionAnalyzer(endPoints, ep)
     this.stackAnalyzer = new StackAnalyzer()
-    this.dnode = new DNode(symbol, trace)
+    this.dnode = new DNode(symbol)
+    this.innerAnalysis(visited)
     this.conditionAnalysis(visited)
     this.crossfunctionAnalysis(visited)
   }
@@ -19,6 +21,86 @@ class RegisterAnalayzer {
   toVisitedKey(pc, trackingPos, symbol) {
     return `${pc}:${trackingPos}:${formatSymbol(symbol)}`
   }
+
+  expandLocalVariable(loadVariable, subTrace) {
+    const node = this.dnode.node;
+    node.alias = loadVariable.toString() 
+    node.variable = loadVariable
+    subTrace.eachLocalVariable(({ variable: storeVariable, value: storedValue, traceIdx, pc, epIdx, kTrackingPos, vTrackingPos }) => {
+      /// If it is exactEqual, return true to break forEach loop 
+      if (storeVariable.exactEqual(loadVariable)) {
+        const dnode = new DNode(storedValue, subTrace)
+        node.childs.push(dnode)
+        return true
+      }
+      if (storeVariable.partialEqual(loadVariable)) {
+        const members = [
+          ...loadVariable.getSymbolMembers(),
+          ...storeVariable.getSymbolMembers(),
+          storedValue,
+        ]
+        members.forEach(m => {
+          const dnode = new DNode(m, subTrace)
+          node.childs.push(dnode)
+        })
+      }
+    })
+  }
+
+  expandStateVariable(loadVariable, subTrace) {
+    const node = this.dnode.node
+    node.alias = loadVariable.toString() 
+    node.variable = loadVariable
+    subTrace.eachStateVariable(({ variable: storeVariable, value: storedValue}) => {
+      /// If it is exactEqual, return true to break forEach loop 
+      if (storeVariable.exactEqual(loadVariable)) {
+        const dnode = new DNode(storedValue, subTrace)
+        node.childs.push(dnode)
+        return true
+      }
+      if (storeVariable.partialEqual(loadVariable)) {
+        const members = [
+          ...loadVariable.getSymbolMembers(),
+          ...storeVariable.getSymbolMembers(),
+          storedValue,
+        ]
+        members.forEach(m => {
+          const dnode = new DNode(m, subTrace)
+          node.childs.push(dnode)
+        })
+      }
+    })
+  }
+
+  innerAnalysis(visited) {
+    const { me, childs } = this.dnode.node
+    assert(!childs.length)
+    switch (me[1]) {
+      case 'MLOAD': {
+        const subTrace = this.trace.sub(me[4][1].toNumber())
+        const loadVariable = toLocalVariable(me[2], subTrace)
+        assert(loadVariable)
+        this.expandLocalVariable(loadVariable, subTrace)
+        break
+      }
+      case 'SLOAD': {
+        const subTrace = this.trace.sub(me[3][1].toNumber())
+        const loadVariable = toStateVariable(me[2], subTrace) 
+        this.expandStateVariable(loadVariable, subTrace)
+        break
+      }
+      default: {
+        const symbols = findSymbol(me, ([type, name]) => ['SLOAD', 'MLOAD'].includes(name))
+        symbols.forEach(symbol => {
+          const traceSize = symbol[1] == 'SLOAD' ? symbol[3] : symbol[4]
+          assert(isConst(traceSize))
+          const dnode = new DNode(symbol, this.trace.sub(traceSize[1].toNumber()));
+          childs.push(dnode)
+        })
+      }
+    }
+  }
+
 
   crossfunctionAnalysis(visited) {
     const sloads = this.dnode.findSloads()
