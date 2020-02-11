e259348bfc22b04f5804b117746d7621800606f3
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-02-11 23:13:41 +0800

    load one more value

--- src/variable/local.js
+++ src/variable/local.js
@@ -4,6 +4,7 @@ const Variable = require('./variable')
 const {
   prettify,
   isConst,
+  isConstWithValue,
   isMload,
 } = require('../shared')
 
@@ -42,27 +43,25 @@ const splitVariable = (t, trace, trackingPos, epIdx) => {
   const item = { t, trace, trackingPos, epIdx }
   const stackOfSymbols = [item]
   const stackOfVariables = []
-  const mloadIndexes = []
   while (stackOfSymbols.length > 0) {
     const { t, trace, trackingPos, epIdx } = stackOfSymbols.pop()
     assert(t && trace && trackingPos >= 0 && epIdx >= 0)
     if (isConst(t)) {
       const variable = new Variable(`m_${t[1].toString(16)}`)
-      stackOfVariables.push({
-        type: 'VAR',
-        value: variable,
-      })
+      stackOfVariables.push({ type: 'VAR', value: variable })
       continue
     }
     if (isMload(t)) {
       const [loc, loadSize, loadTraceSize] = t.slice(2)
+      assert(isConst(loc))
       assert(isConst(loadTraceSize))
-      stackOfSymbols.push({ t: loc, trace, trackingPos, epIdx })
-      stackOfVariables.push({
-        type: 'MLOAD',
-        value: { t, trace, trackingPos, epIdx }
+      const subTrace = trace.filter(t => {
+        if (t[1] != 'MSTORE') return false
+        return isConstWithValue(t[2], loc[1].toNumber())
       })
-      mloadIndexes.push(stackOfVariables.length - 1)
+      const { t: symbol } = subTrace.last()
+      assert(isConst(symbol[3]))
+      stackOfSymbols.push({ t: symbol[3], trace, trackingPos, epIdx })
       continue
     }
     const properties = []
@@ -81,45 +80,25 @@ const splitVariable = (t, trace, trackingPos, epIdx) => {
       value: reverse(properties),
     })
   }
-  stackOfVariables.reverse()
-  let mloadCounter = 0
-  for (let i = 0; i < stackOfVariables.length; i++) {
-    const { type } = stackOfVariables[i]
-    if (type == 'MLOAD') mloadCounter ++
-    if (mloadCounter == 2) {
-      return [
-        stackOfVariables.slice(0, i),
-        stackOfVariables.slice(i),
-      ]
-    }
-  }
-  return [
-    stackOfVariables,
-    [],
-  ]
+  return stackOfVariables
 }
 
 const toLocalVariable = (t, trace, trackingPos, epIdx) => {
-  const [stackOfVariables, others] = splitVariable(t, trace, trackingPos, epIdx) 
+  const stackOfVariables = splitVariable(t, trace, trackingPos, epIdx) 
   let v = null
   while (stackOfVariables.length > 0) {
-    const { value, type } = stackOfVariables.shift()
-    if (type == 'VAR') {
-      v = value
-      continue
-    }
-    if (type == 'PROP') {
-      v.addN(value)
-      continue
+    const { value, type } = stackOfVariables.pop()
+    switch (type) {
+      case 'VAR': {
+        v = value
+        break
+      }
+      case 'PROP': {
+        v.addN(value)
+        break
+      }
     }
   }
-  // if (others.length > 0) {
-    // const [{ type, value: { t, trace, trackingPos, epIdx }}] = others
-    // const [loc, loadSize, loadTraceSize] = t.slice(2)
-    // const subTrace = trace.sub(loadTraceSize[1].toNumber())
-    // subTrace.eachLocalVariable(({ variable: storeVariable, value: storedValue, traceIdx }) => {
-    // })
-  // }
   return v
 }
 module.exports = toLocalVariable
