c125e79283fc321a04e4283d9120254b3371ef4f
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-02-16 01:06:42 +0800

    add markerStack

--- src/variable/local.js
+++ src/variable/local.js
@@ -12,9 +12,13 @@ const sumAll = (constSymbols) => constSymbols.reduce((r, n) => r + n[1].toNumber
 
 const toLocalVariables = (t, trace, trackingPos, epIdx) => {
   if (isConst(t)) return [new Variable(`m_${t[1].toString(16)}`)]
+
   const variables = []
-  const bases = []
+  const baseStack = []
   const mainStack = [t]
+  const markerStack = [{ baseIdx: 0, propIdx: 0 }]
+  const propStack = []
+
   while (mainStack.length > 0) {
     const t = mainStack.pop()
     const [name, ...params] = t.slice(1)
@@ -23,17 +27,14 @@ const toLocalVariables = (t, trace, trackingPos, epIdx) => {
         assert(false, 'Should handle MLOAD here')
       }
       case 'MUL': {
-        const v = new Variable(`m_${sumAll(bases).toString(16)}`)
-        const prop = { symbol: t, trackingPos, epIdx }
-        v.addN([prop])
-        variables.push(v)
+        propStack.push({ symbol: t, trackingPos, epIdx })
         break
       }
       case 'ADD': {
         const constIndex = findIndex(params, (param) => isConst(param))
         assert(constIndex != -1)
         const t = params[1 - constIndex] 
-        bases.push(params[constIndex])
+        baseStack.push(params[constIndex])
         mainStack.push(t)
         break
       }
@@ -42,8 +43,17 @@ const toLocalVariables = (t, trace, trackingPos, epIdx) => {
       }
     }
   }
+
+  while (markerStack.length > 0) {
+    const { baseIdx, propIdx } = markerStack.pop()
+    const bases = baseStack.splice(baseIdx)
+    const props = propStack.splice(propIdx)
+    const v = new Variable(`m_${sumAll(bases).toString(16)}`) 
+    v.addN(props)
+    variables.push(v)
+  }
+
   assert(variables.length > 0)
-  /// TODO: return all variables, not the first member
   return variables
 }
 module.exports = toLocalVariables
