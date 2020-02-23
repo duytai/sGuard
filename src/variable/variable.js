95a04ffbad900d96afca8a16fbe8956263765ce0
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-02-23 16:33:10 +0800

    no more variable members

--- src/variable/variable.js
+++ src/variable/variable.js
@@ -4,52 +4,18 @@ const chalk = require('chalk')
 const { uniq } = require('lodash')
 const { logger, isConst } = require('../shared')
 
-/// FIXME: a member is a sum of all other constant members 
-/// to compare between two variable
 class Variable  {
   constructor(root) {
     assert(root)
     this.root = root
-    this.members = [] 
-  }
-
-  addN(ms) {
-    assert(ms.length > 0)
-    ms.forEach(({ trackingPos, epIdx, symbol }) => {
-      assert(trackingPos >= 0 && epIdx >= 0 && !!symbol)
-    })
-    this.members = [...this.members, ...ms]
   }
 
   toString() {
-    const prop = this.members.map(({ symbol }) => isConst(symbol) ? symbol[1].toString(16) : '*').join('.')
-    return [this.root, prop].filter(p => !!p).join('.')
+    return this.root
   }
 
   prettify() {
     logger.debug(chalk.green.bold(this.toString()))
-  }
-
-  getMembers() {
-    return [...this.members]
-  }
-
-  exactEqual(other) {
-    const myStr = this.toString()
-    const otherStr = other.toString()
-    return myStr == otherStr && !myStr.includes('*')
-  }
-
-  partialEqual(other) {
-    const minLen = Math.min(this.members.length, other.members.length)
-    for (let i = 0; i < minLen; i ++) {
-      const member = this.members[i]
-      const otherMember = other.members[i]
-      if (isConst(member) && isConst(otherMember)) {
-        if (member[1].toNumber() != otherMember[1].toNumber()) return false
-      }
-    }
-    return this.root == other.root
   }
 }
 
