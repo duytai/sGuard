63fe97b11f5de2bd1077aa1e76337d9638193513
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-02-24 23:35:17 +0800

    add sample bin file

--- stress/index.js
+++ stress/index.js
@@ -2,11 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const assert = require('assert')
 const Web3 = require('web3')
-const { Evm, Stack, ExecutionPath } = require('../src/evm')
-const { Trace } = require('../src/trace')
+const { Evm } = require('../src/evm')
 const { logger } = require('../src/shared')
 const Analyzer = require('../src/analyzer')
-const Oracle = require('../src/oracle')
 
 const binFolder = path.join(__dirname, 'bin/')
 const binFiles = fs.readdirSync(binFolder).sort()
@@ -14,10 +12,6 @@ const allowedContract = process.env.CONTRACT
 
 const main = async() => {
   if (allowedContract.startsWith('0x')) {
-    const stack = new Stack()
-    const ep = new ExecutionPath()
-    const trace = new Trace()
-    const pc = 0
     const web3 = new Web3('https://mainnet.infura.io/v3/6f9974d98d0941629d72a2c830f47ecd')
     const code = await web3.eth.getCode(allowedContract)
     assert(code, 'Code must exist')
@@ -25,37 +19,12 @@ const main = async() => {
     logger.info(`binLengh: ${bin.length}`)
     const evm = new Evm(Buffer.from(bin, 'hex'))
     logger.info('>> Start evm')
-    evm.execute(pc, stack, ep, trace)
-    logger.info('>> Start analyzer')
-    const { checkPoints, endPoints } = evm
-    logger.info(`>> endPoints   : ${endPoints.length}`)
-    logger.info(`>> checkPoints : ${checkPoints.length}`)
-    checkPoints.forEach(({ type, data }) => {
-      switch (type) {
-        case 'CALL': {
-          const analyzer = new Analyzer(data, endPoints)
-          const oracle = new Oracle(analyzer)
-          analyzer.prettify()
-          const bugNames = [
-            'BLOCK_DEP',
-            'TIME_DEP',
-          ]
-          oracle.findBugs(bugNames).forEach(dep => {
-            dep.report()
-          })
-          break
-        }
-      }
-    })
+    const { checkPoints, endPoints } = evm.start()
     logger.info(`>> endPoints   : ${endPoints.length}`)
     logger.info(`>> checkPoints : ${checkPoints.length}`)
   } else {
     startFrom = parseInt(allowedContract)
     for (let i = startFrom; i < binFiles.length; i++) {
-      const stack = new Stack()
-      const ep = new ExecutionPath()
-      const trace = new Trace()
-      const pc = 0
       const binFile = binFiles[i]
       assert(binFile, 'binFile must exist')
       logger.info(`binFile ${binFile}`)
@@ -64,28 +33,7 @@ const main = async() => {
       logger.info(`idx: ${i}`)
       const evm = new Evm(Buffer.from(bin, 'hex'))
       logger.info('>> Start evm')
-      evm.execute(pc, stack, ep, trace)
-      logger.info('>> Start analyzer')
-      const { checkPoints, endPoints } = evm
-      logger.info(`>> endPoints   : ${endPoints.length}`)
-      logger.info(`>> checkPoints : ${checkPoints.length}`)
-      checkPoints.forEach(({ type, data }) => {
-        switch (type) {
-          case 'CALL': {
-            const analyzer = new Analyzer(data, endPoints)
-            const oracle = new Oracle(analyzer)
-            analyzer.prettify()
-            const bugNames = [
-              'BLOCK_DEP',
-              'TIME_DEP',
-            ]
-            oracle.findBugs(bugNames).forEach(dep => {
-              dep.report()
-            })
-            break
-          }
-        }
-      })
+      const { checkPoints, endPoints } = evm.start()
       logger.info(`>> endPoints   : ${endPoints.length}`)
       logger.info(`>> checkPoints : ${checkPoints.length}`)
     }
