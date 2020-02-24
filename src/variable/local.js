83fb2a8c593e5a0f33075d36ca8e45fdda531607
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-02-24 17:10:22 +0800

    possible locs

--- src/variable/local.js
+++ src/variable/local.js
@@ -10,7 +10,8 @@ const {
 
 class LocalVariable {
   constructor(t, ep) {
-    this.convert(t, ep)
+    const locs = this.convert(t, ep)
+    prettify(locs)
   }
 
   findArraySize(ep) {
@@ -22,45 +23,31 @@ class LocalVariable {
     return right[1].toNumber()
   }
 
-  findUniqMemstores(ep) {
-    ep.trace.prettify()
-    console.log('----')
-    ep.trace.filter(({ t }) => {
-      prettify([t])
-    })
-  }
-
   convert(t, ep) {
     if (isConst(t)) return [new Variable(`m_${t[1].toString(16)}`)]
 
     const mainStack = [t]
-    const pointerStack = []
-    const propStack = []
-    const markerStack = []
+    const markerStack = [ep.size()]
+    let concreteLocs = []
 
     while (mainStack.length > 0) {
       const t = mainStack.pop()
       switch (t[0]) {
         case 'const': {
-          pointerStack.push(t)
+          concreteLocs.push(t)
           break
         }
         default: {
           switch(t[1]) {
             case 'MLOAD': {
               const [loc, loadSize, _, epSize] = t.slice(2)
-              markerStack.push({
-                pointerIdx: pointerStack.length,
-                propIdx: propStack.length,
-                epSize,
-              })
+              markerStack.push(epSize[1].toNumber())
               mainStack.push(loc)
               break
             }
             case 'ADD': {
               const operands = t.slice(2)
               assert(operands[0][1] == 'MUL')
-              propStack.push(operands[0])
               mainStack.push(operands[1])
               break
             }
@@ -73,19 +60,24 @@ class LocalVariable {
     }
 
     while (markerStack.length > 0) {
-      const { pointerIdx, propIdx, epSize } = markerStack.pop() 
-      const [pointer] = pointerStack.splice(pointerIdx)
-      const [prop] = propStack.splice(propIdx)
-      const subEp = ep.sub(epSize[1].toNumber())
-      assert(pointer && prop)
-      const storedValue = subEp.trace.memValueAt(pointer)
-      /// Find array size to detect possible overlap with previous data segment 
+      const epSize = markerStack.pop() 
+      const subEp = ep.sub(epSize)
       const arraySize = this.findArraySize(subEp)
-      console.log(`s: ${arraySize}`)
-      this.findUniqMemstores(subEp)
-      assert(false)
+      const tmp = []
+      concreteLocs.forEach(concreteLoc => {
+        for (let i = 0; i < arraySize; i ++) {
+          const loc = ['const', concreteLoc[1].add(new BN(0x20 * i))]
+          if (ep.size() == epSize) {
+            tmp.push(loc)
+          } else {
+            tmp.push(ep.trace.memValueAt(loc))
+          }
+        }
+      })
+      concreteLocs = tmp
     }
-    return []
+
+    return concreteLocs
   }
 }
 
