01d59a6b22e7770822e4bac1ef44bcc432f7a9ab
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-02-27 16:16:08 +0800

    update dnode in register

--- src/analyzer/register.js
+++ src/analyzer/register.js
@@ -1,33 +1,38 @@
 const assert = require('assert')
-const { intersection, uniqBy, uniq, assign, forEach } = require('lodash')
 const DNode = require('./dnode')
 const ConditionAnalyzer = require('./condition') 
 const StackAnalyzer = require('./stack')
-const { prettify, formatSymbol, findSymbol, isConst, toVisitedKey } = require('../shared')
+const { prettify, findSymbol } = require('../shared')
 const { StateVariable, LocalVariable } = require('../variable')
 
 class Register {
   constructor(symbol, ep, endPoints) {
-    assign(this, { dnode: new DNode(symbol), ep, endPoints })
-    this.internalAnalysis(symbol)
+    this.dnode = new DNode(symbol)
+    this.ep = ep
+    this.endPoints = endPoints
+    this.internalAnalysis(symbol, this.dnode)
   }
 
-  internalAnalysis(symbol) {
+  internalAnalysis(symbol, dnode) {
+    assert(symbol && dnode)
     switch (symbol[1]) {
       case 'MLOAD': {
         const epSize = symbol[5][1].toNumber()
         const ep = this.ep.sub(epSize)
         const localVariable = new LocalVariable(symbol[2], ep)
+        dnode.addChild(new DNode(symbol))
         break
       }
       case 'SLOAD': {
-        const epSize = symbol[5][1].toNumber()
+        const epSize = symbol[3][1].toNumber()
         const ep = this.ep.sub(epSize)
         const stateVariable = new StateVariable(symbol[2], ep)
+        dnode.addChild(new DNode(symbol))
         break
       }
       default: {
         const symbols = findSymbol(symbol, ([type, name]) => ['SLOAD', 'MLOAD'].includes(name))
+        symbols.forEach(symbol => this.internalAnalysis(symbol, dnode))
       }
     }
   }
