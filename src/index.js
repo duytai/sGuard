a5cc5698b9120547251e075a38886c3dfc7047e7
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-03-03 13:38:24 +0800

    better package.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 const assert = require('assert')
 const fs = require('fs')
+const path = require('path')
+const shell = require('shelljs')
+const dotenv = require('dotenv')
+const chalk = require('chalk')
 const { Evm } = require('./evm')
 const { logger } = require('./shared')
 const { forEach } = require('lodash')
 const Analyzer = require('./analyzer')
 
-assert(process.env.COMPILED)
-const compiled = fs.readFileSync(process.env.COMPILED, 'utf8')
+const { parsed: { contract } } = dotenv.config()
+assert(contract, 'require contract in .env')
+const pwd = shell.pwd().toString()
+const contractFile = path.join(pwd, contract)
+assert(fs.existsSync(contractFile), 'contract must exist')
+const jsonFile = `${contractFile}.json`
 
-forEach(JSON.parse(compiled).contracts, (contractJson, name) => {
+logger.info(`display compiler version`)
+shell.exec(`solc --version`)
+logger.info(`compile ${chalk.green.bold(contract)} by using env compiler`)
+shell.exec(`solc --combined-json asm,bin-runtime ${contractFile} > ${jsonFile}`)
+assert(fs.existsSync(jsonFile), 'json must exist')
+
+const output = fs.readFileSync(jsonFile, 'utf8')
+forEach(JSON.parse(output).contracts, (contractJson, name) => {
   const bin = Buffer.from(contractJson['bin-runtime'], 'hex')
   const evm = new Evm(bin)
-  console.log(`++++++++++ Analyzing contract: ${name} ++++++++++`)
+  logger.info(`Start Analyzing Contract: ${chalk.green.bold(name.split(':')[1])}`)
   const { checkPoints, endPoints } = evm.start()
   checkPoints.forEach(ep => {
     const analyzer = new Analyzer(ep, endPoints)
