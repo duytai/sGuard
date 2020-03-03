ad754285000840f720eb1fd5284723e7af6fd486
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-03-03 13:58:47 +0800

    update stress test

--- stress/loader.js
+++ stress/loader.js
@@ -3,11 +3,18 @@ const cheerio = require('cheerio')
 const Web3 = require('web3')
 const fs = require('fs')
 const path = require('path')
+const dotenv = require('dotenv')
+
+const { parsed : { etherscanFrom, etherscanTo } } = dotenv.config() 
+const from = parseInt(etherscanFrom)
+const to = parseInt(etherscanTo)
 
 const main = async() => {
-  let counter = 1; 
   const web3 = new Web3('https://mainnet.infura.io/v3/6f9974d98d0941629d72a2c830f47ecd')
-  for (let i = 0; i < 50; i ++) {
+  const binFolder = path.join(__dirname, 'bin')
+  if (!fs.existsSync(binFolder)) fs.mkdirSync(binFolder)
+  let counter = 1; 
+  for (let i = from; i < to; i ++) {
     const pageURL = `https://etherscan.io/contractsVerified/${i + 1}`
     const htmlString = await rp.get(pageURL)
     const $ = cheerio.load(htmlString)
