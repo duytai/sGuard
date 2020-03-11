4e0d39a52e8737f2cb1fa54a7da1cca5be5bf5b8
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-03-11 20:04:53 +0800

    report one time

---
+++ src/vul/disorder.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const { formatSymbol } = require('../shared')
+const Oracle = require('./oracle')
+
+class Disorder extends Oracle {
+  startFinding() {
+    const ret = []
+    // const stack = [this.dnode]
+    // while (stack.length) {
+      // const dnode = stack.pop()
+      // const { node: { me, childs } } = dnode
+      // const txt = formatSymbol(me)
+      // if (txt.includes('NUMBER') || txt.includes('TIMESTAMP')) ret.push(dnode)
+      // childs.forEach(child => stack.push(child))
+    // }
+    return ret
+  }
+}
+
+module.exports = Disorder
