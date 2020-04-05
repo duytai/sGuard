3a9ff7a265c1c3fd1728d2f23560cba8cf906692
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-04-05 15:43:08 +0800

    add subtract

--- src/vul/index.js
+++ src/vul/index.js
@@ -1,15 +1,13 @@
 const assert = require('assert')
 const { toPairs } = require('lodash')
 const { prettify, logger, gb } = require('../shared')
-const Integer = require('./integer')
-const UncheckReturnValue = require('./uncheck')
+const Subtract = require('./subtract')
 
 class Scanner {
   constructor(cache, srcmap, ast) {
     this.srcmap = srcmap
     this.vuls = {
-      integer: new Integer(cache, srcmap, ast),
-      uncheck: new UncheckReturnValue(cache, srcmap, ast),
+      subtract: new Subtract(cache, srcmap, ast),
     }
   }
 
@@ -21,28 +19,9 @@ class Scanner {
         ...(this.vuls[k].scan() || [])
       ]
     }
-    this.fix(bugFixes)
   }
 
   fix(bugFixes) {
-    // Apply bugFixes
-    bugFixes.sort((x, y) => x.start - y.start)
-    let acc = 0
-    let source = this.srcmap.source
-    bugFixes.forEach(bugFix => {
-      bugFix.start = bugFix.start + acc
-      // Begin fix here
-      const first = source.slice(0, bugFix.start + 1)
-      const last = source.slice(bugFix.start + 1)
-      logger.info(`PATCH: ${gb(bugFix.check.trim())}`)
-      source = [first, bugFix.check, last].join('')
-      // End fix
-      acc += bugFix.len
-    })
-    if (bugFixes.length) {
-      logger.info(`FIXED`)
-      console.log(source)
-    }
   }
 
 } 
---
+++ src/vul/subtract.js
@@ -0,0 +1,69 @@
+const assert = require('assert')
+const { toPairs } = require('lodash')
+const { 
+  formatWithoutTrace: formatSymbol,
+  findSymbols,
+  findOperands
+} = require('../shared')
+const Tree = require('./tree')
+
+class Subtract {
+  constructor(cache, srcmap, ast) {
+    this.cache = cache
+    this.srcmap = srcmap
+    this.ast = ast
+  }
+
+  generateCheckPoints(dnode) {
+    assert(dnode)
+    const checkPoints = {}
+    const { endPoints } = this.cache
+    const { node: { me, endPointIdx } } = dnode
+    const nodes = findSymbols(me, ([_, name]) => name == 'SUB')
+    nodes.forEach(node => {
+      const [left, right, epSize] = node.slice(2)
+      if (left[1] == 'ISZERO' || right[1] == 'ISZERO') return
+      const expression = formatSymbol(node)
+      const epIdx = epSize[1].toNumber() - 1
+      const endPoint = endPoints[endPointIdx]
+      const { pc, opcode } = endPoint.get(epIdx)
+      assert(opcode.name == 'SUB')
+      const operands = findOperands(pc, this.srcmap, this.ast)
+      checkPoints[expression] = { pc, operands }
+    })
+    return checkPoints
+  }
+
+  findUncheckOperands(tree, endPoints) {
+    assert(tree && endPoints)
+    const uncheckOperands = {}
+    const dnodes = tree.root.traverse(({ node: { me } }) => formatSymbol(me).includes('SUB('))
+    dnodes.forEach(dnode => {
+      const checkPoints = this.generateCheckPoints(dnode)
+      for (const t in checkPoints) {
+        const { operands, pc } = checkPoints[t]
+        uncheckOperands[pc] = operands
+      }
+    })
+    return uncheckOperands
+  }
+
+  generateBugFixes(operands) {
+    console.log(operands)
+  }
+
+  scan() {
+    const { mem: { calls }, endPoints } = this.cache
+    const tree = new Tree(this.cache)
+    calls.forEach((call, endPointIdx) => {
+      toPairs(call).forEach(([epIdx, value]) => {
+        tree.build(endPointIdx, epIdx, value)
+      })
+    })
+    const uncheckOperands = this.findUncheckOperands(tree, endPoints)
+    const bugFixes = this.generateBugFixes(uncheckOperands)
+    return []
+  }
+} 
+
+module.exports = Subtract
