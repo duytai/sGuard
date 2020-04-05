d4ee976af9a2d905261dee6fa8dcc18132d233fe
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-04-05 18:48:21 +0800

    merge sub and add

--- src/vul/addition.js
+++ src/vul/addition.js
@@ -7,7 +7,7 @@ const {
 } = require('../shared')
 const Tree = require('./tree')
 
-class Subtract {
+class Addition {
   constructor(cache, srcmap, ast) {
     this.cache = cache
     this.srcmap = srcmap
@@ -19,7 +19,7 @@ class Subtract {
     const checkPoints = {}
     const { endPoints } = this.cache
     const { node: { me, endPointIdx } } = dnode
-    const nodes = findSymbols(me, ([_, name]) => name == 'SUB')
+    const nodes = findSymbols(me, ([_, name]) => name == 'ADD')
     nodes.forEach(node => {
       const [left, right, epSize] = node.slice(2)
       if (left[1] == 'ISZERO' || right[1] == 'ISZERO') return
@@ -27,9 +27,9 @@ class Subtract {
       const epIdx = epSize[1].toNumber() - 1
       const endPoint = endPoints[endPointIdx]
       const { pc, opcode } = endPoint.get(epIdx)
-      assert(opcode.name == 'SUB')
+      assert(opcode.name == 'ADD')
       const operands = findOperands(pc, this.srcmap, this.ast)
-      operands.type = 'SUB'
+      operands.type = 'ADD'
       checkPoints[expression] = { pc, operands }
     })
     return checkPoints
@@ -38,7 +38,7 @@ class Subtract {
   findUncheckOperands(tree, endPoints) {
     assert(tree && endPoints)
     const uncheckOperands = {}
-    const dnodes = tree.root.traverse(({ node: { me } }) => formatSymbol(me).includes('SUB('))
+    const dnodes = tree.root.traverse(({ node: { me } }) => formatSymbol(me).includes('ADD('))
     dnodes.forEach(dnode => {
       const checkPoints = this.generateCheckPoints(dnode)
       for (const t in checkPoints) {
@@ -62,4 +62,4 @@ class Subtract {
   }
 } 
 
-module.exports = Subtract
+module.exports = Addition
--- src/vul/index.js
+++ src/vul/index.js
@@ -1,26 +1,76 @@
 const assert = require('assert')
-const { toPairs } = require('lodash')
+const { toPairs, sortBy } = require('lodash')
 const { prettify, logger, gb } = require('../shared')
 const Subtract = require('./subtract')
+const Addition = require('./addition')
 
 class Scanner {
   constructor(cache, srcmap, ast) {
     this.srcmap = srcmap
     this.vuls = {
       subtract: new Subtract(cache, srcmap, ast),
+      addition: new Addition(cache, srcmap, ast),
     }
   }
 
   scan() {
-    let bugFixes = []
+    let uncheckOperands = []
     for (const k in this.vuls) {
-      bugFixes = [
-        ...bugFixes,
+      uncheckOperands = [
+        ...uncheckOperands,
         ...(this.vuls[k].scan() || [])
       ]
     }
+    const bugFixes = this.generateBugFixes(uncheckOperands)
     this.fix(bugFixes)
   }
+
+  generateBugFixes(uncheckOperands) {
+    const bugFixes = []
+    const pairs = sortBy(uncheckOperands, [
+      (x) => x[1].range[0],
+      (y) => - y[1].range[1],
+    ])
+    for (const idx in pairs) {
+      const [pc, { range, operands, type }] = pairs[idx]
+      switch (type) {
+        case 'ADD':
+        case 'SUB': {
+          const [from, to] = range
+          const func = type.toLowerCase()
+          const check = `${func}(${operands.map(x => x.id).join(', ')})`
+          const diff = check.length - (to - from)
+          const [left, right] = operands 
+          for (let next = parseInt(idx) + 1; next < pairs.length; next ++) {
+            const nextRange = pairs[next][1].range
+            let isModified = false 
+            // Left operand
+            if (nextRange[0] >= left.range[0] && nextRange[1] <= left.range[1] && !isModified) {
+              nextRange[0] += 4 
+              nextRange[1] += 4 
+              isModified = true
+            }
+            // Right operand
+            if (nextRange[0] >= right.range[0] && nextRange[1] <= right.range[1] && !isModified) {
+              nextRange[0] += 2 - (right.range[0] - left.range[1]) + 4
+              nextRange[1] += 2 - (right.range[0] - left.range[1]) + 4
+              isModified = true
+            }
+            // After both operands
+            if (nextRange[0] >= to && !isModified) {
+              nextRange[0] += diff
+              nextRange[1] += diff
+              isModified = true
+            }
+          }
+          bugFixes.push({ from, to, check })
+          break
+        }
+      }
+    }
+    return bugFixes
+  }
+
 
   fix(bugFixes) {
     let source = this.srcmap.source
