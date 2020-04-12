811066dfd732570206b334cb3d9c72173a603374
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-04-12 16:51:02 +0800

    cache by expression and pc

--- src/vul/integer/addition.js
+++ src/vul/integer/addition.js
@@ -25,13 +25,14 @@ class Addition {
     nodes.forEach(node => {
       const [left, right, epSize] = node.slice(2)
       if (left[1] == 'ISZERO' || right[1] == 'ISZERO') return
+      const expression = formatSymbol(node)
       const epIdx = epSize[1].toNumber() - 1
       const endPoint = endPoints[endPointIdx]
       const { pc, opcode } = endPoint.get(epIdx)
       assert(opcode.name == 'ADD')
       const operands = findOperands(pc, this.srcmap, this.ast)
       if (!operands.operator) return
-      checkPoints[pc] = { pc, operands }
+      checkPoints[expression + pc] = { pc, operands }
     })
     return checkPoints
   }
