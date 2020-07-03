000d825052818c761fecb8b6ff27c6b4cf30542c
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-07-03 21:16:52 +0800

    fix isConst

--- src/shared/index.js
+++ src/shared/index.js
@@ -2,9 +2,11 @@ const assert = require('assert')
 const logger = require('./logger')
 const prettify = require('./prettify')
 const searching = require('./searching')
+const isConst = t => t[0] == 'const'
 
 module.exports = {
   logger,
+  isConst,
   ...prettify,
   ...searching,
 }
